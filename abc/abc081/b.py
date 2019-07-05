48e9dc34226b2c5d90794c48edafc70ab4f9404d
Jumpei Takegata <takegata.j@gmail.com>
2019-07-05 23:27:23 +0900

    ABC081_B

---
+++ abc/abc081/b.py
@@ -0,0 +1,18 @@
+def shiftOnly():
+	n = int(input())
+	A = list(map(int,input().split()))
+	count = 0
+	while True:
+		for a in A:
+			flag = True
+			if a % 2 != 0:
+				flag = False
+				break
+		if flag:
+			count += 1
+			A = [a/2 for a in A]
+		else:
+			break
+	print(count)
+
+shiftOnly()
